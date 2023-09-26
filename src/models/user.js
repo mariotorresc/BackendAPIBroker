4efc655d84fb4e60e3cf4642c5c80e27cb65b639
Mario Torres <mariotorres@MacBook-Air-de-Felipe.local>
2023-09-26 11:54:42 -0300

    feat(bycrypt): removed

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-param-reassign */
 const { Model } = require('sequelize');
-const bcrypt = require('bcrypt');
 
 module.exports = (sequelize, DataTypes) => {
   class user extends Model {}
@@ -54,31 +53,6 @@ module.exports = (sequelize, DataTypes) => {
     user.hasMany(models.userStock, {
       foreignKey: 'userId',
     });
-  };
-
-  user.beforeBulkCreate(async (users) => {
-    const newUsers = Promise.all(
-      users.map(async (us) => {
-        if (us.changed('password')) {
-          us.password = await bcrypt.hash(us.password, 10);
-        }
-        if (us.changed('email')) {
-          us.email = us.email.toLowerCase();
-          const user1 = await user.findOne({ where: { email: us.email } });
-          if (user1) {
-            const error = new Error('There is another account with this email');
-            error.number = 422;
-            throw error;
-          }
-        }
-      })
-    );
-    return newUsers;
-  });
-
-  // PROTOTYPE FUNCTIONS
-  user.prototype.checkPassword = function checkPassword(password) {
-    return bcrypt.compare(password, this.password);
   };
   return user;
 };
