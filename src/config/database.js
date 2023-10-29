65cdf26e2ec3b4fac1ee0ac26150cfc9787eac3d
Mario Torres <mariotorres@MacBook-Air-de-Felipe.local>
2023-10-29 02:05:36 -0300

    feat(db var)

--- src/config/database.js
+++ src/config/database.js
@@ -8,19 +8,6 @@ const config = {
     password: process.env.DB_PASSWORD || 'mypassword',
     username: process.env.DB_USERNAME || 'myuser',
   },
-  development: {
-    database: process.env.DB_NAME || 'iic2513template_dev',
-    extend: 'default',
-  },
-  production: {
-    extend: 'default',
-    // eslint-disable-next-line camelcase
-    use_env_variable: 'DATABASE_URL',
-  },
-  test: {
-    database: 'iic2513template_test',
-    extend: 'default',
-  },
 };
 
 Object.keys(config).forEach((configKey) => {
