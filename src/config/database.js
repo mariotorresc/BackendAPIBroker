53d7f137ae8df0c46db49cb24553d154d8ca6518
Mario Torres <mariotorres@MacBook-Air-de-Felipe.local>
2023-10-29 01:59:14 -0300

    feat(db var)

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 const config = {
   default: {
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME || 'brokerdb',
     dialect: process.env.DB_DIALECT || 'postgres',
     host: 'db',
-    password: process.env.DB_PASSWORD,
-    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD || 'mypassword',
+    username: process.env.DB_USERNAME || 'myuser',
   },
   development: {
     database: process.env.DB_NAME || 'iic2513template_dev',
